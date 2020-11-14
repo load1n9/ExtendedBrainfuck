0df1e4896142ca8f8c7f54989d8898050f5209a3
loading <dsrebnik@gmail.com>
2020-11-14 22:44:12 +0000

    changes

--- example/test.ts
+++ example/test.ts
@@ -1,4 +1,4 @@
-import { BrainFuck } from "../bf.ts"
+import { BrainFuck } from "../dist/bf.ts"
 let code = `++++++++++[>+++++++>++++++++++>+++>+<<<<-]>++.>+.+++++++..+++.>++.<<+++++++++++++++.>.+++.------.--------.>+.>.`
 let program = new BrainFuck(code)
 console.log(program.Output())
